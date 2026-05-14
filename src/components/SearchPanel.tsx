c1558c627e6eb5b367e50abfe04e39795e2c2716
Juan-Vera-ok <juanvera2001@hotmail.com>
2026-05-14 17:39:25 -0300

    Add search panel

---
+++ src/components/SearchPanel.tsx
@@ -0,0 +1,8 @@
+import "./SearchPanel.css";
+export const SearchPanel = () => {
+    return (
+        <div className="root">
+            <input type="text" autoFocus placeholder="Buscar periodo o evento..." className=" --border input" />
+        </div>
+    )
+};
